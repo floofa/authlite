1a137d572fff7c7e74f7e0717b350dc975820cb4
Ondrej Trojanek <ondrej.trojanek@gmail.com>
2012-01-13 10:49:47 +0100

    init

---
+++ config/authlite.php
@@ -0,0 +1,43 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+	/**
+	 * User model
+	 */
+	'user_model' => 'user',
+	
+	/**
+	 * Username column
+	 */
+	'username' => 'login_name',
+	
+	/**
+	 * Password column
+	 */
+	'password' => 'password',
+	
+	/**
+	 * Session column
+	 */
+	'session' => 'session',
+	
+	/**
+	 * Type of hash to use for passwords. Any algorithm supported by the hash function
+	 * can be used here.
+	 * @see http://php.net/hash
+	 * @see http://php.net/hash_algos
+	 */
+	'hash_method' => 'md5',
+	
+	/**
+	 * Set the auto-login (remember me) cookie lifetime, in seconds. The default
+	 * lifetime is two weeks.
+	 */
+	'lifetime' => 1209600,
+	
+	/**
+	 * Set the session key that will be used to store the current user.
+	 */
+	'session_key' => 'authlite_user',
+);
